169faa3f3921e286cd4046dfebe58a7c88c8bb64
E. Schleicher <jthschleicher@gmail.com>
2026-01-16 22:16:01 +0100

    ui_textures

--- scenes/main_ui_shell.gd
+++ scenes/main_ui_shell.gd
@@ -28,7 +28,24 @@ func create_unit_buttons(units):
 		
 		# Mindestgröße
 		button.custom_minimum_size = Vector2(200, 40)
+		
+		var spritesheet: Texture2D = preload("res://assets/Sprout Lands - UI Pack - Basic pack/Sprite sheets/Sprite sheet for Basic Pack.png")
+		
+		
+		var atlas := AtlasTexture.new()
+		atlas.atlas = spritesheet
+		atlas.region = Rect2(163, 178, 90, 27) # X, Y, Breite, Höhe
+		
+		var style := StyleBoxTexture.new()
+		style.texture = atlas
 
+		# Ränder (für Skalierung wichtig!)
+		style.set_texture_margin(SIDE_LEFT, 4)
+		style.set_texture_margin(SIDE_RIGHT, 4)
+		style.set_texture_margin(SIDE_TOP, 4)
+		style.set_texture_margin(SIDE_BOTTOM, 4)
+		
+		button.add_theme_stylebox_override("normal", style)
 		# Optional: Theme / Style
 		#button.add_theme_font_size_override("font_size", 18)
 
