785c2197c10450a7bf520809c951f76a50230de7
J.-Th. Schleicher <jthschleicher@gmail.com>
2026-01-22 14:27:09 +0100

    yay

---
+++ scenes/camera_2d.gd
@@ -0,0 +1,25 @@
+extends Camera2D
+
+@export var zoom_speed: float = 0.1
+@export var min_zoom: float = 0.5
+@export var max_zoom: float = 3.0
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			zoom -= Vector2(zoom_speed, zoom_speed)
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			zoom += Vector2(zoom_speed, zoom_speed)
+
+		# Begrenzen
+		zoom.x = clamp(zoom.x, min_zoom, max_zoom)
+		zoom.y = clamp(zoom.y, min_zoom, max_zoom)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
