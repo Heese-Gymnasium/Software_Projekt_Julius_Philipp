5e76174966860ac260499c9dc13f4d9b422d0f94
Philipp Brockmann <philipp.brockmann@b-sz-gyst.logoip.de>
2026-01-23 08:47:43 +0100

    yay

--- scripts/unit.gd
+++ scripts/unit.gd
@@ -1,7 +1,26 @@
 extends Node
 
-var x
-var y
+var _x
+var _y
+
+var x:
+	get:
+		_x
+	set(value):
+		_x = value
+		
+var y:
+	get:
+		_y
+	set(value):
+		_y = value
+
+
+func spawn(spawn_x, spawn_y):
+	x = spawn_x
+	y = spawn_y
+	pos_x = 
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
