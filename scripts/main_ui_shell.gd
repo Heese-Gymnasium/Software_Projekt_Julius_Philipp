0f170e0774e6aed3f9f0cc6197df91dc3cc03c3b
E. Schleicher <jthschleicher@gmail.com>
2026-02-28 17:34:04 +0100

    refactoring in main UI shell, jede unit ruft nun fähigkeiten ab, allerdings sibt es bisher nur beispieldaten

--- scripts/main_ui_shell.gd
+++ scripts/main_ui_shell.gd
@@ -6,13 +6,14 @@ extends MarginContainer
 @onready var unit_objekt := $ScrollContainer3/Unit_object_Ui
 @onready var unit_main_parent := $ScrollContainer
 @onready var cards_main_parent := $ScrollContainer2
+@onready var unit_sub_menue := $ScrollContainer3
 @onready var unit := self.get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_node("root_tile").get_node("TileMapLayer").get_node("trgt_unit")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_parent().show()
 	show()
-	on_turn_start()
+	_on_turn_start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,18 +21,19 @@ func _process(delta: float) -> void:
 	pass
 
 
-func on_turn_start():
+func _on_turn_start():
 	var active_player = get_active_player()
-	var cards = get_cards(active_player)
-	create_card_buttons(cards)
-	var units = get_units(active_player)
-	create_unit_buttons(units)
+	var cards = _get_cards(active_player)
+	_create_buttons(cards_main, cards)
+	var units = _get_units(active_player)
+	_create_buttons(unit_main, units)
 	unit_main_parent.show() 
 	cards_main_parent.hide()
+	unit_sub_menue.hide()
 	main_active = 0
 
-func open_menue(unit):
-	var abiletys = get_abilitys(unit)
+func _open_menue(unit):
+	var abiletys = _get_abilitys(unit)
 	for abilety in abiletys:
 		var button := Button.new()
 		button.add_theme_color_override("font_color", Color("#009f8e"))
@@ -73,12 +75,16 @@ func open_menue(unit):
 		unit_objekt.add_child(button)
 	
 func _handle_button_impact_units(unit):
-	open_menue(unit)
-	unit_main_parent.show() 
+	unit_main_parent.hide() 
 	cards_main_parent.hide()
+	unit_sub_menue.show()
+	_create_buttons(unit_objekt, unit)
 	main_active = 2
+
+	
 func _on_units_pressed() -> void:
 	unit_main_parent.show() 
+	unit_sub_menue.hide()
 	cards_main_parent.hide()
 	main_active = 0
 
@@ -86,110 +92,13 @@ func _on_units_pressed() -> void:
 func _on_cards_pressed() -> void:
 	unit_main_parent.hide()
 	cards_main_parent.show()
+	unit_sub_menue.hide()
 	main_active = 1
-	
 
-func create_unit_buttons(units):
-	
-	unit_main.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	unit_main.size_flags_vertical = Control.SIZE_EXPAND_FILL
-	# Alte Buttons löschen (falls Units sich ändern)
-	for child in unit_main.get_children():
-		child.queue_free()
-
-	for unit in units:
-		var button := Button.new()
-		button.add_theme_color_override("font_color", Color("#009f8e"))
-		button.add_theme_color_override("font_focus_color", Color("#009f8e"))
-		button.add_theme_color_override("font_hover_color", Color.WHITE)
-		button.add_theme_color_override("font_pressed_color", Color.WHITE)
-		button.add_theme_color_override("font_disabled_color", Color.GRAY)
-
-		# Text
-		button.text = unit.name + " (HP: %d)" % unit.hp
-		
-		# Mindestgröße
-		button.custom_minimum_size = Vector2(350, 40)
-		button.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
-		button.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
-		
-		var spritesheet: Texture2D = preload("res://assets/Sprout Lands - UI Pack - Basic pack/Sprite sheets/Sprite sheet for Basic Pack.png")
-		
-		
-		var atlas := AtlasTexture.new()
-		atlas.atlas = spritesheet
-		atlas.region = Rect2(163, 178, 90, 27) # X, Y, Breite, Höhe
-		
-		var style := StyleBoxTexture.new()
-		style.texture = atlas
-
-		# Ränder (für Skalierung wichtig!)
-		style.set_texture_margin(SIDE_LEFT, 4)
-		style.set_texture_margin(SIDE_RIGHT, 4)
-		style.set_texture_margin(SIDE_TOP, 4)
-		style.set_texture_margin(SIDE_BOTTOM, 4)
-		
-		button.add_theme_stylebox_override("normal", style)
-
-		# Signal verbinden
-		#button.pressed.connect(_on_unit_button_pressed.bind(unit))
-
-		unit_main.add_child(button)
-		print(button.get_parent())
-
-
-
-func create_card_buttons(cards):
-	cards_main.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	cards_main.size_flags_vertical = Control.SIZE_EXPAND_FILL
-	
-	# Alte Buttons löschen (falls Units sich ändern)
-	for child in unit_main.get_children():
-		child.queue_free()
-
-	for card in cards:
-		var button := Button.new()
-		button.add_theme_color_override("font_color", Color("#009f8e"))
-		button.add_theme_color_override("font_focus_color", Color("#009f8e"))
-		button.add_theme_color_override("font_hover_color", Color.WHITE)
-		button.add_theme_color_override("font_pressed_color", Color.WHITE)
-		button.add_theme_color_override("font_disabled_color", Color.GRAY)
-
-		# Text
-		button.text = card.name + " (Cost: %d)" % card.cost
-		
-		# Mindestgröße
-		button.custom_minimum_size = Vector2(350, 40)
-		button.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
-		button.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
-
-		
-		var spritesheet: Texture2D = preload("res://assets/Sprout Lands - UI Pack - Basic pack/Sprite sheets/Sprite sheet for Basic Pack.png")
-		
-		
-		var atlas := AtlasTexture.new()
-		atlas.atlas = spritesheet
-		atlas.region = Rect2(163, 178, 90, 27) # X, Y, Breite, Höhe
-		
-		var style := StyleBoxTexture.new()
-		style.texture = atlas
-
-		# Ränder (für Skalierung wichtig!)
-		style.set_texture_margin(SIDE_LEFT, 4)
-		style.set_texture_margin(SIDE_RIGHT, 4)
-		style.set_texture_margin(SIDE_TOP, 4)
-		style.set_texture_margin(SIDE_BOTTOM, 4)
-		
-		button.add_theme_stylebox_override("normal", style)
-
-		# Signal verbinden
-		#button.pressed.connect(_on_unit_button_pressed.bind(unit))
-
-		cards_main.add_child(button)
 func get_active_player() -> int:
 	return 0
 
-func get_units(active_player):
+func _get_units(active_player):
 	var units = [
 	{"name": "Soldat", "hp": 100},
 	{"name": "Bogenschütze", "hp": 70},
@@ -199,7 +108,7 @@ func get_units(active_player):
 	{"name": "Magier", "hp": 50}]
 	return units
 	
-func get_cards(active_player):
+func _get_cards(active_player):
 	var cards = [
 	{"name": "Card1", "cost": 100},
 	{"name": "Card2", "cost": 70},
@@ -209,5 +118,71 @@ func get_cards(active_player):
 	{"name": "Card3", "cost": 50}]
 	return cards
 
-func get_abilitys(unit):
-	return unit.get_skills_base(unit)
+func _get_abilitys(unit):
+	# return unit.get_skills_base(unit)
+	return ["test1", "test2"]
+	
+
+func _create_buttons(target, objekts):
+	if(objekts is Dictionary):
+		objekts = _get_abilitys(objekts)
+	target.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	target.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	
+	# Alte Buttons löschen (falls Units sich ändern)
+	for child in target.get_children():
+		child.queue_free()
+
+	for objekt in objekts:
+		var button := Button.new()
+		button.add_theme_color_override("font_color", Color("#009f8e"))
+		button.add_theme_color_override("font_focus_color", Color("#009f8e"))
+		button.add_theme_color_override("font_hover_color", Color.WHITE)
+		button.add_theme_color_override("font_pressed_color", Color.WHITE)
+		button.add_theme_color_override("font_disabled_color", Color.GRAY)
+
+		# Text
+		if(objekt is String):
+			button.text = objekt
+		elif(objekt.has("hp")):
+			button.text = objekt.name + " (Hp: %d)" % objekt.hp
+		elif(objekt.has("cost")):
+			button.text = objekt.name + " (Cost: %d)" % objekt.cost
+		else:
+			button.text = "objekt not found"
+		
+		
+		# Mindestgröße
+		button.custom_minimum_size = Vector2(350, 40)
+		button.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+		button.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+
+		
+		var spritesheet: Texture2D = preload("res://assets/Sprout Lands - UI Pack - Basic pack/Sprite sheets/Sprite sheet for Basic Pack.png")
+		
+		
+		var atlas := AtlasTexture.new()
+		atlas.atlas = spritesheet
+		atlas.region = Rect2(163, 178, 90, 27) # X, Y, Breite, Höhe
+		
+		var style := StyleBoxTexture.new()
+		style.texture = atlas
+
+		# Ränder (für Skalierung wichtig!)
+		style.set_texture_margin(SIDE_LEFT, 4)
+		style.set_texture_margin(SIDE_RIGHT, 4)
+		style.set_texture_margin(SIDE_TOP, 4)
+		style.set_texture_margin(SIDE_BOTTOM, 4)
+		
+		button.add_theme_stylebox_override("normal", style)
+
+		# Signal verbinden
+		#button.pressed.connect(_on_unit_button_pressed.bind(unit))
+		button.pressed.connect(_handel_sub_input.bind(objekt))
+		target.add_child(button)
+	
+func _handel_sub_input(objekt):
+	if(objekt.has("hp")):
+		_handle_button_impact_units(objekt)
+	elif(objekt.has("cost")):
+		pass
